4890ad0da4815eeada0f0d11acc6322b19e61c56
jaishruti <shrutijaiswal223@gmail.com>
2025-06-25 12:05:14 +0530

    token generation

--- backend/src/config/utils.js
+++ backend/src/config/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
     maxAge: 3*24*60*60*1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    // secure: process.env.NODE_ENV !== "development",
   });
 
   return token;
