97aa70830c6624217bb03f233217e824579baeb6
jaishruti <shrutijaiswal223@gmail.com>
2025-06-25 13:01:51 +0530

    update profile checkauth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -72,4 +72,31 @@ export const logout = (req, res) => {
   }
 };
 
-export
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic) {
+      return res.status(400).json({ message: "profile picture missing" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+  } catch (error) {
+    console.log("error in logout controller", error.message);
+    res.status(500).json({ message: "internal error" });
+  }
+};
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
